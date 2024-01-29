e52528d47e115693dcd5f20f77e9bb772817e0c6
mohsinchd <mohsinshoaib735@gmail.com>
2024-01-29 12:32:55 +0500

    modified: Image Avatar changed

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/mohsin.jpg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
@@ -64,7 +64,11 @@ function Home2() {
 
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img
+                src={myImg}
+                className="img-fluid rounded-circle"
+                alt="avatar"
+              />
             </Tilt>
           </Col>
         </Row>
